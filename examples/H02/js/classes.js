a1fd36867019810976f970dafb082c6f2bf04257
Peter Kassenaar <info@kassenaar.com>
2019-12-12 19:52:53 +0100

    Updated class example

--- examples/H02/js/classes.js
+++ examples/H02/js/classes.js
@@ -1,19 +1,21 @@
 // classes
 
-
-const empOfTheYear = new Employee('Maikel');
-console.log(empOfTheYear.name); //
-
-
-class Person{
+// Base class
+class Person {
 	constructor(props) {
 		this.name = props
 	}
-
 }
-class Employee extends Person{
-	// private salary=10000
+
+// Extended class
+class Employee extends Person {
+	// private salary=10000; // JavaScript doesn't know about 'private' and 'public'
 	constructor(props) {
 		super(props); // call of the constructor of the BASE class
 	}
 }
+
+// No hoisting of classes, so this code needs to be down here
+const empOfTheYear = new Employee('Maikel');
+console.log(empOfTheYear.name); // Maikel
+
